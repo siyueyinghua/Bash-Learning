cd1618713a4ca4f6016e196675f9e13b83f496b1
siyueyinghua <18345179649@163.com>
2019-10-19 01:50:10 +0800

    add the loglevelbetter.sh for debugging more conveniencely, renew stack.sh with loglevelbetter method

---
+++ src/logging/loglevelbetter.sh
@@ -0,0 +1,340 @@
+#!/bin/bash
+
+#********************************************************************
+#            Copyright (C) wuyou Technologies, 2012-3
+#
+#  #FileName                : setuplog.sh
+#  #Writter                 : wuyou
+#  #Sub Modules             :
+#  #Mudule Name             : LOG
+#  #Language                : shell
+#  #Environment             : 2285 + Suse Linux SP1
+#  #First Release Date      : 2012-3-15
+#  #Description             : This script contains the MiniOS Log
+#                             interfaces and generic Utility functions.
+#********************************************************************
+
+#templogfile
+#LOG_FILE="/var/log/bashdbug.log"
+#export LOG_FILE
+
+#const value of different loglevel
+CON_EMERG="0"
+CON_ALERT="1"
+CON_CRIT="2"
+CON_ERROR="3"
+CON_WARN="4"
+CON_NOTICE="5"
+CON_INFO="6"
+CON_DEBUG="7"
+
+#set default log level
+DEFAULT_LEVEL="7"
+LOG_LEVEL=${DEFAULT_LEVEL}
+DEFAULT_LOGFILE="./defaultBashDebug.log"
+LOG_FILE=${DEFAULT_LOGFILE}
+DEFAULT_LOG_2TERM2="0"
+LOG_2TERM2=${DEFAULT_LOG_2TERM2}
+
+CURRENT_TTY=`tty`
+if [ $? -ne 0 ];then
+    CURRENT_TTY="/dev/console"
+fi
+#echo $CURRENT_TTY
+
+#####################################################################
+# Function Name: g_LOG_Init
+# Description  : MiniOS Init Log interface
+# Parameter    : 1) move logfile? default is move, 0 = no move
+# Date         : <03-04-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Init()
+{
+    #local loglevel=`cat /proc/cmdline | awk -F 'log_level=' '{print $2}' | awk '{print $1}'`
+    local isDel
+    local loglevel
+    local logfile
+    local log2term2
+
+    # handle the options
+    # the option is as follows:
+    # [-m move-or-not] [-l log-level] [-d log-directory] [-t log-also-2-terminal]'
+    while getopts ":m:l:d:t:" option
+    do
+        case ${option} in
+            m )
+                isDel=${OPTARG}
+                #echo the option isDel is: ${isDel}
+                ;;
+            l )
+                loglevel=${OPTARG}
+                #echo the option loglevel is: ${loglevel}
+                ;;
+            d )
+                logfile=${OPTARG}
+                #echo The option logfile is: ${logfile}
+                ;;
+            t )
+                log2term2=${OPTARG}
+                #echo The option logfile is: ${log2term2}
+                ;;
+            ? )
+                echo 'right usage: g_LOG_Init [-m move-or-not] [-l log-level] [-d log-directory] [-t log-also-2-terminal]'
+                ;;
+            * )
+                echo "The case default"
+                ;;
+        esac
+    done
+
+    # set the log file
+    if [ -z "${logfile}" ]
+    then
+        echo "Warning: input logfile is null! the logfile will be set to default: ${LOG_FILE}!" >> ${LOG_FILE}
+    else
+        LOG_FILE=${logfile}
+        echo "Info: get the input logfile! the logfile will be set to it: ${logfile}!" >> ${LOG_FILE}
+    fi
+
+    local logdir="`dirname ${LOG_FILE}`"
+    
+    if [ "$isDel" = "1" ]
+    then
+        if [ -f "${LOG_FILE}" ]
+        then
+            rm -rf ${LOG_FILE}
+        fi
+
+        #set logdir
+        if [ ! -d "${logdir}" ]
+        then
+            mkdir -p ${logdir}
+        fi
+        touch ${LOG_FILE}
+    fi
+
+    #set loglevel
+    if [ -z "${loglevel}" ]
+    then
+        echo "Warning: input loglevel is null! the loglevel will be set to default: ${DEFAULT_LEVEL}!" >> ${LOG_FILE}
+    elif [ ! `echo ${loglevel} | grep '^[0-7]$'` ]
+    then
+        echo "Warning: input loglevel is invalid! the loglevel will be set to default: ${DEFAULT_LEVEL}!" >> ${LOG_FILE}
+    else
+        echo "Info: input loglevel is valid! the loglevel will be set to it: ${loglevel}!" >> ${LOG_FILE}
+        LOG_LEVEL=${loglevel}
+    fi
+
+    # set the log2trem
+    if [ -z "${log2term2}" ]
+    then
+        echo "Warning: input log2term2 is null! the log2term2 will be set to default: ${LOG_2TERM2}!" >> ${LOG_FILE}
+    else
+        LOG_2TERM2=${log2term2}
+        echo "Info: get the input log2term2! the log2term2 will be set to it: ${log2term2}!" >> ${LOG_FILE}
+    fi
+
+    echo "current log level is ${LOG_LEVEL}." >> ${LOG_FILE}
+    chmod 640 ${LOG_FILE}
+    return 0
+}
+
+#####################################################################
+# Function Name: LOG_WriteLog
+# Description  : MiniOS Log interface for installation
+# Parameter    : 1) Severity of the log
+#                2) Line number of log message
+#                3) Log message
+# Date         : <03-04-2012>
+# Author       : 
+#####################################################################
+function LOG_WriteLog()
+{
+    local progname=`basename ${BASH_SOURCE[2]}`
+    local severity=$1
+    local line_no=$2
+    shift 2
+    local logmsg=$@
+
+    if [ ${severity} -le ${LOG_LEVEL} ]
+    then
+        case ${severity} in
+            0)  #Emerg log
+                echo -n "[ EMERG ] - " >> ${LOG_FILE}
+                ;;
+            1)  #Alert log
+                echo -n "[ ALERT ] - " >> ${LOG_FILE}
+                ;;
+            2)  #crit log
+                echo -n "[ CRIT ] - " >> ${LOG_FILE}
+                ;;
+            3)  # Error log
+                echo -n "[ ERROR ] - " >> ${LOG_FILE}
+                ;;
+            4)  # Warning log
+                echo -n "[ WARN ] - " >> ${LOG_FILE}
+                ;;
+            5)  #Notice log
+                echo -n "[ NOTICE ] - " >> ${LOG_FILE}
+                ;;
+            6)  # Information log
+                echo -n "[ INFO ] - " >> ${LOG_FILE}
+                ;;
+            7)  # Debug log
+                echo -n "[ DEBUG ] - " >> ${LOG_FILE}
+                ;;
+            *)  # Any other logs
+                echo -n "[       ] - " >> ${LOG_FILE}
+                ;;
+        esac
+        echo "`date "+%Y-%m-%d %H:%M:%S"`" ${progname} "[line_no=${line_no}]:" \
+            "${logmsg}" >> ${LOG_FILE}
+    fi
+
+    if [ ${LOG_2TERM2} = "1" ]
+    then
+        if [ ${severity} -le ${LOG_LEVEL} ]
+        then
+            case ${severity} in
+                0)  #Emerg log
+                    echo -n "[ EMERG ] - "
+                    ;;
+                1)  #Alert log
+                    echo -n "[ ALERT ] - "
+                    ;;
+                2)  #crit log
+                    echo -n "[ CRIT ] - "
+                    ;;
+                3)  # Error log
+                    echo -n "[ ERROR ] - "
+                    ;;
+                4)  # Warning log
+                    echo -n "[ WARN ] - "
+                    ;;
+                5)  #Notice log
+                    echo -n "[ NOTICE ] - "
+                    ;;
+                6)  # Information log
+                    echo -n "[ INFO ] - "
+                    ;;
+                7)  # Debug log
+                    echo -n "[ DEBUG ] - "
+                    ;;
+                *)  # Any other logs
+                    echo -n "[       ] - "
+                    ;;
+            esac
+            echo -e "`date "+%Y-%m-%d %H:%M:%S"`" ${progname} "[line_no=${line_no}]:" \
+                "${logmsg}"
+        fi
+    fi
+
+    return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Emerg
+# Description  : MiniOS Emerg Log interface
+# Parameter    : 1) Emerg message
+# Date         : <05-18-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Emerg()
+{
+    LOG_WriteLog $CON_EMERG $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Alert
+# Description  : MiniOS Alert Log interface
+# Parameter    : 1) Alert message
+# Date         : <05-18-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Alert()
+{
+    LOG_WriteLog $CON_ALERT $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Crit
+# Description  : MiniOS Crit Log interface
+# Parameter    : 1) Crit message
+# Date         : <05-18-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Crit()
+{
+    LOG_WriteLog $CON_CRIT $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Error
+# Description  : MiniOS Error Log interface
+# Parameter    : 1) Error message
+# Date         : <03-04-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Error()
+{
+    LOG_WriteLog $CON_ERROR $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Warn
+# Description  : MiniOS Warning Log interface
+# Parameter    : 1) Warning message
+# Date         : <03-04-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Warn()
+{
+    LOG_WriteLog $CON_WARN $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Notice
+# Description  : MiniOS Notice Log interface
+# Parameter    : 1) Notice message
+# Date         : <05-18-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Notice()
+{
+    LOG_WriteLog $CON_NOTICE $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Info
+# Description  : MiniOS Information Log interface
+# Parameter    : 1) Info  message
+# Date         : <03-04-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Info()
+{
+    LOG_WriteLog $CON_INFO $BASH_LINENO "$@"
+	return 0
+}
+
+#####################################################################
+# Function Name: g_LOG_Debug
+# Description  : MiniOS Debug Log interface
+# Parameter    : 1) Debug  message
+# Date         : <03-04-2012>
+# Author       : 
+#####################################################################
+function g_LOG_Debug()
+{
+    LOG_WriteLog $CON_DEBUG $BASH_LINENO "$@"
+	return 0
+}
+
+g_LOG_Init $@
--- src/stack/stack.sh
+++ src/stack/stack.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
 inputfile="../../data/symbol.txt"
-declare -a stack
+source "../logging/loglevelbetter.sh"
+#log_file="/var/log/yesirelincoln/bashdebug.log"
+log_file="./bashdebug.log"
 
 function push
 {
     stack+=( $* )
-    #echo ${#stack[@]}
-    echo "push" $*
-    #echo ${#stack[@]}
+    g_LOG_Debug before push, the contents of the stack are: ${stack[@]}
+    g_LOG_Debug "push" $*
+    g_LOG_Debug after push, the contents of the stack are: ${stack[@]}
 }
 
 pop()
 {
-    let size=${#stack[@]}-1
-    #echo the stack size is: $size
-    #echo ${#stack[@]}
-    #echo ${stack[@]}
-    #echo "pop" ${stack[$size]}
-    unset stack[$size]
+    let size=${#stack[@]}
+    g_LOG_Debug the stack size is: $size
+    g_LOG_Debug before pop, the contents of the stack are: ${stack[@]}
+    g_LOG_Debug now will "pop" the stack top: ${stack[$size-1]}
+    unset stack[$size-1]
+    g_LOG_Debug after pop, the contents of the stack are: ${stack[@]}
 }
+
+declare -a stack
+
+g_LOG_Init -m 1 -l 5 -d ${log_file} -t 1
+if [ $? -eq 0 ]
+then
+    g_LOG_Debug "g_LOG_Init successfully!"
+else
+    g_LOG_Debug "failed to g_LOG_Init, please check!"
+fi
 
 #ensure the stack to be empty
 while [[ ${#stack[@]} -gt 0 ]]
@@ -29,70 +41,71 @@ done
 
 openSymbol="([<{"
 closeSymbol=")]>}"
+g_LOG_Debug $openSymbol
+g_LOG_Debug $closeSymbol
+
 declare -A symbolarr
 symbolarr[")"]="("
 symbolarr["]"]="["
 symbolarr[">"]="<"
 symbolarr["}"]="{"
-#echo ${symbolarr[@]}
-#echo ${!symbolarr[@]}
-#echo ${symbolarr["("]}
-#echo open_close
-#echo $openSymbol
-#echo $closeSymbol
+g_LOG_Debug symbolarr: ${symbolarr[@]}
+g_LOG_Debug num symbolarr: ${!symbolarr[@]}
+g_LOG_Debug "the value by the key '}': ${symbolarr["}"]}"
 
 while read -r line
 do
-    echo the expr is: $line
+    g_LOG_Notice the readed-in expr is: $line
+
     idx_begin=0
     idx_end=0
-    arr=( `echo "$line" | grep -o .` )
+    #**# the set -f means noglob, so that bash will not do pathname expansion,
+    #**# such as "* expanded to any path name, ~ expanded to home directory"
+    set -f
+    tmpArr=( `echo "$line" | grep -o .` )
+    g_LOG_Debug "Testing if the splliting string method is effective? the result is: ${tmpArr[@]}"
+    set +f
 
-    echo ${arr[@]}
     for ch in `echo "$line" | grep -o .`
     do
         let idx_begin++
-        #echo $ch
+        g_LOG_Debug "loop, now to handle the ch: $ch"
         if [[ ${openSymbol} =~ "${ch}" ]] 
         then
-            #echo find a opensymbol: $ch push it to stack
+            g_LOG_Debug find a opensymbol: $ch push it to stack
             push $ch
         elif [[ ${closeSymbol} =~ "$ch" ]]
         then
             if [[ ${#stack[@]} -eq 0 ]]
             then
-                echo -e "\033[31m!!!error, is a wrong symbol expr\033[0m"
+                g_LOG_Error "\033[31m!!!error, $line is a wrong symbol expr\033[0m"
                 break
             else
-                #echo find a closeSymbol: $ch
-                let size=${#stack[@]}-1
-                #echo the stack size is: $(($size+1))
-                #echo the index $ch is: ${symbolarr["$ch"]}
-                if [[ ${symbolarr[$ch]} != ${stack[$size]} ]]
+                g_LOG_Debug find a closeSymbol: $ch
+                let size=${#stack[@]}
+                g_LOG_Debug the key $ch is: ${symbolarr["$ch"]}
+                if [[ ${symbolarr[$ch]} != ${stack[$size-1]} ]]
                 then
-                    echo do not match, will not continue to manipulate
-                    echo -e "\033[31m!!!error, is a wrong symbol expr\033[0m"
+                    g_LOG_Debug do not match, will not continue to manipulate
+                    g_LOG_Error "\033[31m!!!error, $line is a wrong symbol expr\033[0m"
                     break
                 fi
                 #no matter wether it match or not, need to pop the former symbol
                 pop
             fi
         else
-            echo this is not a open or close symbol: $ch
+            g_LOG_Debug this is not a open or close symbol: $ch, drop it, go on!
         fi
         let idx_end++
     done
     if [[ ${#stack[@]} -eq 0 ]] && [[ $idx_begin -eq $idx_end ]]
     then
-        echo -e "\033[32mright, this is a right symbol expr!!!\033[0m"
+        g_LOG_Notice "\033[32mright, $line is a right symbol expr!!!\033[0m"
     fi
     while [[ ${#stack[@]} -gt 0 ]]
     do
         pop
     done
 done < "$inputfile"
-
-
-echo ${stack[@]} 
 
 exit $?
